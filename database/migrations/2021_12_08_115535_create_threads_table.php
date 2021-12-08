eb42881b392a0fd8da6d46b09becd868be5a91db
satoshi <kanimo3104@gmail.com>
2021-12-08 21:10:40 +0900

    コントローラー、table,modelの作成（骨組みのみ）

---
+++ database/migrations/2021_12_08_115535_create_threads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->bigIncrements('thread_id');
+            $table->string('name',30)->default('名無し様');
+            $table->text('body');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
