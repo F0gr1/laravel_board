05373144b1423a0652ef8f6ec87f90d3b19e0ab6
satoshi <kanimo3104@gmail.com>
2021-12-15 17:31:54 +0900

    DB設計図に従ってテーブル定義

--- app/Models/Response.php
+++ app/Models/Response.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ThreadContent extends Model
+class Response extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'body',
+        'name','message'
     ];
 }
---
+++ database/migrations/2021_12_15_075455_create_responses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("thread_id");
+            $table->foreign('thread_id')->references('id')->on('threads');
+            $table->string('name', 100);
+            $table->text('message');
+            $table->timestamp('posted_at');
+            $table->integer('ip_addr');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+}
